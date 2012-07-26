9439a269b93d5d7fba113f4915f75d48c3bf5507
serzh <serzh@bankophone.com.ua>
2012-07-26 11:57:15 +0300

    add ability for converting some documents with flag -c

--- tokindle.sh
+++ tokindle.sh
@@ -4,16 +4,34 @@
 SMTP_HOST='smtp.gmail.com:587'
 SMTP_USER='example@gmail.com'
 FROM='example@gmail.com'
-TO='example@gmail.com'
+TO='example@kindle.com'
+SUBJECT=''
+CONVERT_SUBJECT='convert'
 
-read -s -p "Enter password: " PASSWORD
-echo "" | \
-    mailx -v -s "" -a "$1" \
-    -S smtp-use-starttls \
-    -S ssl-verify=ignore \
-    -S smtp-auth=login \
-    -S smtp="$SMTP_HOST" \
-    -S smtp-auth-user="$SMTP_USER" \
-    -S smtp-auth-password="$PASSWORD" \
-    -S from="$FROM" \
-    $TO
+read -s -p "Enter SMTP password: " PASSWORD
+
+while [ $# -gt 0 ]
+do
+    SUBJECT=''
+
+    if [ $1 = "-c" ]
+    then
+        SUBJECT=$CONVERT_SUBJECT
+        shift
+    fi
+    
+    echo "\nSending $1..."
+
+    echo "" | \
+        mailx -s "$SUBJECT" -a "$1" \
+        -S smtp-use-starttls \
+        -S ssl-verify=ignore \
+        -S smtp-auth=login \
+        -S smtp="$SMTP_HOST" \
+        -S smtp-auth-user="$SMTP_USER" \
+        -S smtp-auth-password="$PASSWORD" \
+        -S from="$FROM" \
+        $TO
+
+    shift
+done
